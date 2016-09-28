9363dc4fd7106d3a15bc709b6c299bfa744e34c6
dhananjay <harel.dhananjay@gmail.com>
2016-09-28 19:13:31 +0530

    New Changes

--- androidUSBSerialMonitorLite/src/main/java/com/my/app/ResultsActivity.java
+++ androidUSBSerialMonitorLite/src/main/java/com/my/app/ResultsActivity.java
@@ -1,15 +1,20 @@
 package com.my.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.app.Activity;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,32 +23,47 @@ import java.util.List;
 public class ResultsActivity extends Activity {
     ListView list;
     private List<ResultModel> results = new ArrayList<ResultModel>();
-
-
+    private SharedPreferences mPrefs;
+    private Editor prefsEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-        results.add( new ResultModel("sam","3","11 June 2013 10.00am"));
-        results.add( new ResultModel("sam","6","11 Aug 2013 8.00am"));
-        results.add( new ResultModel("sam","11","5 Jan 2014 10.00am"));
-        results.add( new ResultModel("sam","12","8 July 2014 2.00pm"));
-        results.add( new ResultModel("sam","15","21 Oct 2014 10.00am"));
-        results.add( new ResultModel("sam","17","11 Dec 2014 10.00am"));
-        results.add( new ResultModel("sam","17","11 Dec 2014 10.00pm"));
-        results.add( new ResultModel("sam","18","11 Jan 2015 10.00am"));
-        results.add( new ResultModel("sam","20","11 Mar 2015 180.00am"));
-        results.add( new ResultModel("sam","21","11 May 2015 10.00am"));
-        results.add( new ResultModel("sam","22","11 July 2015 11.00am"));
-        results.add( new ResultModel("sam","26","11 Nov 2015 7.00am"));
-        results.add( new ResultModel("sam","26","11 Nov 2015 10.00am"));
-        results.add( new ResultModel("sam","34","11 March 2016 10.00am"));
-        results.add( new ResultModel("sam","40","11 July 2016 10.00am"));
-        results.add( new ResultModel("sam","41","11 Aug 2016 10.00am"));
-        results.add( new ResultModel("sam","41","11 Aug 2016 11.00am"));
-        results.add( new ResultModel("sam","41","11 Aug 2016 12.00pm"));
-        results.add( new ResultModel("sam","41","11 Aug 2016 1.00pm"));
-        results.add( new ResultModel("sam","41","11 Aug 2016 7.00pm"));
+         mPrefs  = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+
+        String newResultModelEntry = mPrefs.getString("newEntry","");
+        try {
+            if (newResultModelEntry.length() > 1) {
+                ResultModel rm = ResultModel.getObjectFromJson(newResultModelEntry);
+                results.add(rm);
+            }
+        }
+        catch (Exception e){
+
+        }
+
+
+        results.add( new ResultModel("Lucas","3","11 June 2013 10.00am",11,22,11.4));
+        results.add( new ResultModel("Oliver","6","11 Aug 2013 8.00am",11,22,11.4));
+        results.add( new ResultModel("Mason","11","5 Jan 2014 10.00am",14,43,11.4));
+        results.add( new ResultModel("Aiden","12","8 July 2014 2.00pm",11,22,11.4));
+        results.add( new ResultModel("Aiden","15","21 Oct 2014 10.00am",22,43,11.4));
+        results.add( new ResultModel("Aiden","17","11 Dec 2014 10.00am",31,42,28));
+        results.add( new ResultModel("Emma","17","11 Dec 2014 10.00pm",17,22,11.4));
+        results.add( new ResultModel("Emma","18","11 Jan 2015 10.00am",14,22,11.4));
+        results.add( new ResultModel("Harper","20","11 Mar 2015 180.00am",23,55,11.4));
+        results.add( new ResultModel("sam","21","11 May 2015 10.00am",11,22,11.4));
+        results.add( new ResultModel("sam","22","11 July 2015 11.00am",11,22,11.4));
+        results.add( new ResultModel("sam","26","11 Nov 2015 7.00am",11,22,11.4));
+        results.add( new ResultModel("sam","26","11 Nov 2015 10.00am",11,44,23.8));
+        results.add( new ResultModel("sam","34","11 March 2016 10.00am",16,22,11.4));
+        results.add( new ResultModel("sam","40","11 July 2016 10.00am",11,22,11.4));
+        results.add( new ResultModel("sam","41","11 Aug 2016 10.00am",15,22,11.4));
+        results.add( new ResultModel("sam","41","11 Aug 2016 11.00am",11,22,15.7));
+        results.add( new ResultModel("sam","41","11 Aug 2016 12.00pm",11,22,11.4));
+        results.add( new ResultModel("sam","41","11 Aug 2016 1.00pm",21,22,21.1));
+        results.add( new ResultModel("sam","41","11 Aug 2016 7.00pm",19,34,21.4));
 
         Results adapter = new Results(ResultsActivity.this, R.layout.list_single,results);
         list=(ListView)findViewById(R.id.list);
@@ -58,6 +78,9 @@ public class ResultsActivity extends Activity {
                 Intent intent = new Intent(view.getContext(),ResultDetailsActivity.class);
                 intent.putExtra("Name",rs.getName());
                 intent.putExtra("Age",rs.getAge());
+                intent.putExtra("MinBreathCount",rs.getMinBreathCount());
+                intent.putExtra("MaxBreathCount",rs.getMaxBreathCount());
+                intent.putExtra("AvgBreathCount",rs.getAvgBreathCount());
                 startActivity(intent);
 
             }
